5e0dac10c51e290432ea7e34beb66cea9c5270ba
Pedro <ordep.pedro99@gmail.com>
2024-12-12 01:13:12 -0300

    Adicionando botão de reiniciar

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ function sortear(){
         }
 
         sorteados.push(numero);
+        alterarBotao();
     }
     document.getElementById("resultado").innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados} </label>`;
 }
@@ -24,3 +25,26 @@ function numAleatorio(min, max){
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function alterarBotao(){
+    let botao = document.getElementById('btn-reiniciar');   
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+      } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar(){
+    limparCampos(['quantidade', 'de', 'ate']);
+    alterarBotao();
+}
+
+function limparCampos(ids) {
+    ids.forEach(id => {
+      const campo = document.getElementById(id);
+      if (campo) campo.value = ''; 
+    });
+  }
